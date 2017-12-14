2e18fe2356c017123a5b324da65abd378c846963
JaySoftye <JaySoftye@iMac.local>
2017-12-14 13:01:48 -0500

    Survey Update

--- templates-pages/nedm-survey-confirmation.php
+++ templates-pages/nedm-survey-confirmation.php
@@ -25,9 +25,10 @@
           </div>
         </div>
         <div class="row card-body">
-          <div class="col-md text-center padding">
+          <div class="col-md text-center">
             <h1><strong>Thank you for your submission</strong></h1>
             <p>Your survey information has been sent to your Networking Consulting team for evaluation. A throughout check on the information provided usually takes between 3-5 business days, during which time we will be constructing a detailed report for your review.</p>
+            <a href="/consulting-services/nedm"><button class="button">Back to Consulting Services</button></a>
           </div>
         </div>
       </div>
@@ -37,4 +38,82 @@
   </div>
 </div>
 
-<?php get_template_part( 'lib/nedm-survey-post-footer' ); ?>
+<footer>
+<div class="container"><div class="row">
+
+  <div class="col-md-4">
+    <h3 class="teqBlue">Its all about learning.</h3>
+    <h6><a class="white" data-toggle="collapse" href="#footerProducts" aria-expanded="false" aria-controls="footerProducts">Products &amp; Things &#187;</a></h6>
+    <ul class="footerLinks collapse" id="footerProducts">
+      <li><a href="../pd-platforms/" class="white">PD Platforms</a></li>
+      <li><a href="../instructional-software/" class="white">Instructional Software</a></li>
+      <li><a href="../stem/" class="white">STEM Solutions</a></li>
+      <li><a href="../sight-and-sound/" class="white">Sight and Sound</a></li>
+      <li><a href="../sd-wan/" class="white">SD-Wan from Cloudgenix</a></li>
+    </ul>
+    <h6><a class="white" data-toggle="collapse" href="#footerConcepts" aria-expanded="false" aria-controls="footerConcepts">Consulting Services &#187;</a></h6>
+    <ul class="footerLinks collapse" id="footerConcepts">
+      <li><a href="http://www.teq.com/consulting-services/" class="white">About Teq Consulting</a></li>
+      <li><a href="http://www.teq.com/consulting-services/eils/" class="white">Effective Educational Leadership &amp; Supervision</a></li>
+      <li><a href="http://www.teq.com/consulting-services/nedm/" class="white">Network Enabled Device Management</a></li>
+      <li><a href="http://www.teq.com/consulting-services/si/" class="white">Stem Initiatives</a></li>
+    </ul>
+    <h6><a class="white" data-toggle="collapse" href="#footerServices" aria-expanded="false" aria-controls="footerServices">Support &amp; Services &#187;</a></h6>
+    <ul class="footerLinks collapse" id="footerServices">
+      <li><a href="../support-and-services/" class="white">Service Plans</a></li>
+      <li><a href="../support-and-services/support-resources/" class="white">Product Guides</a></li>
+    </ul>
+    <h6><a class="white" data-toggle="collapse" href="#footerLearning" aria-expanded="false" aria-controls="footerLearning">Community &#187;</a></h6>
+    <ul class="footerLinks collapse" id="footerLearning">
+      <li><a href="/learning-community/teq-talk/" class="white">Teq Talk</a></li>
+      <li><a href="/learning-community/teq-tips/" class="white">Teq Tips</a></li>
+      <li><a href="/learning-community/media-resource-center/" class="white">Media Center</a></li>
+      <li><a href="/learning-community/social-media-center/" class="white">Social Media </a></li>
+    </ul>
+    <h6><a class="white" href="../teq-events/">Events &#187;</a></h6>
+    <h6><a class="white" data-toggle="collapse" href="#footerAbout" aria-expanded="false" aria-controls="footerAbout">About &#187;</a></h6>
+    <ul class="footerLinks collapse" id="footerAbout">
+      <li><a href="../about-us/" class="white">About Us</a></li>
+      <li><a href="../about-us/press-releases/" class="white">Press Releases</a></li>
+      <li><a href="../about-us/testimonials/" class="white">Testimonials</a></li>
+      <li><a href="../about-us/teq-careers/" class="white">Teq Careers</a></li>
+      <li><a href="../contact-us/" class="white">Contact Us</a></li>
+    </ul>
+  </div>
+
+  <div class="col-md-4">
+    <div class="row">
+      <div class="col-md-12">
+        <img src="../consulting-services/../consulting-services/public/images/teq-footer-teq-icon.png" />
+      </div>
+      <div class="col-md-12">
+        <p class="white">7 Norden Lane<br />Huntington Station, NY 11746 United States</p>
+        <p class="white">Phone: 877.455.9369<br />Email: info@teq.com<br />Fax: 631.293.4951</p>
+        <h6><a href="https://www.facebook.com/myTeq/"><img src="../consulting-services/../consulting-services/public/images/teq-footer-facebook-icon.png" /></a><a href="https://twitter.com/TeqPD"><img src="../consulting-services/../consulting-services/public/images/teq-footer-twitter-icon.png" /></a><a href="https://www.linkedin.com/company/teq/"><img src="../consulting-services/../consulting-services/public/images/teq-footer-linkedin-icon.png" /></a></h6>
+        <small class="white">©2017 Teq - all rights reserved</small>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4">
+    <h3 class="teqBlue">Stay Connected</h3>
+    <p class="white">Subscribe to receive email from Teq</p>
+    <!--[if lte IE 8]>
+    <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
+    <![endif]-->
+    <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
+    <script>
+      hbspt.forms.create({
+        portalId: '182596',
+        formId: 'ec8c46db-58d6-4267-9f39-6f3fc85bfc2f'
+      });
+    </script>
+  </div>
+
+</div></div>
+</footer>
+
+<script type="text/javascript" src="/consulting-services/../consulting-services/../consulting-services/public/js/bootstrap.js"></script>
+
+</body>
+</html>
