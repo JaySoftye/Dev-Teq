33a8b9b1ef612327e5492971c810a9e743432187
JaySoftye <jonathansoftye@teq.com>
2019-01-08 14:41:41 -0500

    Updated Careers Posting Layout
    Template update: about-us-teq-careers.php

--- templates-pages/about-us-teq-careers.php
+++ templates-pages/about-us-teq-careers.php
@@ -11,8 +11,8 @@
   </nav>
 
   <main>
-    <div class="container nopadding padding-top">
-      <section id="teqBlog">
+    <div class="container nopadding padding-top padding-bottom">
+      <section class="padding-bottom" id="teqBlog">
         <div class="row">
           <div class="col-md text-center">
             <h1>Interested in a <strong>career at Teq</strong>?</h1>
@@ -40,25 +40,27 @@
           </div>
         </div>
 
-        <div class="row padding-bottom">
-          <div class="col-md-12 text-center padding">
+        <div class="row">
+          <div class="col-md-12 padding-sm">
             <h1>Below is the current list of <strong>available positions</strong> at Teq</h1>
             <p>Click the full description for instructions on how to apply.</p>
-            <hr />
           </div>
-          <?php
-            $the_query = new WP_Query( array( 'category_name' => 'careers', 'posts_per_page' => '12', 'orderby' => 'rand') );
-               while ($the_query -> have_posts()) : $the_query -> the_post();
-          ?>
-
-          <div class="col-md-6">
-            <h6><a href="<?php the_permalink() ?>"><strong><?php the_title(); ?></strong></a></h6>
-            <?php echo get_the_excerpt(); ?> <a class="teqBlue" href="<?php the_permalink() ?>"><strong>Full Description &#187;</strong></a> </p>
-            <hr />
-          </div>
-
-          <?php endwhile; wp_reset_postdata(); ?>
         </div>
+        <?php
+          $the_query = new WP_Query( array( 'category_name' => 'careers', 'posts_per_page' => '-1', 'orderby' => 'rand') );
+            while ($the_query -> have_posts()) : $the_query -> the_post();
+        ?>
+        <div class="row padding-sm">
+          <div class="col-md-10 flex-md-middle">
+            <h6><a href="<?php the_permalink() ?>"><strong><?php the_title(); ?></strong></a></h6>
+            <?php echo get_the_excerpt(); ?>
+          </div>
+          <div class="col-md-2 flex-md-middle">
+            <a href="<?php the_permalink() ?>"><button class="small">See full posting</button></a>
+          </div>
+        </div>
+        <hr />
+        <?php endwhile; wp_reset_postdata(); ?>
 
       </section>
     </div>
