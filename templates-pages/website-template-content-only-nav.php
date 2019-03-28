7b5726f2ba043e368768f7e510bc83ccc3e8db8b
JaySoftye <jonathansoftye@teq.com>
2019-03-28 10:10:38 -0400

    Update website-template-content-only-nav.php
    Update to page template - no 'get' method to call original header - line deleted out "get_header();"

--- templates-pages/website-template-content-only-nav.php
+++ templates-pages/website-template-content-only-nav.php
@@ -5,9 +5,27 @@
  * @package WordPress
  * @subpackage BootstrapWP
  */
-get_header(); ?>
-  <?php get_template_part( 'navigation', 'default' ); ?>
-</nav>
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta http-equiv="content-type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
+    <meta name="description" content="<?php echo esc_attr( get_bloginfo( 'description' ) ); ?>" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+      <?php wp_head(); ?>
+    <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+
+    <!-- Custom CSS -->
+    <link href="<?php echo get_stylesheet_directory_uri();?>/css/teq-brand-style.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo get_stylesheet_directory_uri();?>/css/teq-brand-style-mobile.css" rel="stylesheet" type="text/css">
+    <!-- Roboto -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900" rel="stylesheet" type="text/css">
+
+</head>
+<body <?php body_class(); ?>>
 
 <main>
   <?php wp_reset_query(); the_content(); ?>
