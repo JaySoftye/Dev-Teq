4cd723dc2c7c9c4138bd0ff9983126676f5ce07f
JaySoftye <jonathansoftye@teq.com>
2019-10-04 17:00:40 -0400

    Update functions.php
    nedm survey update

--- functions.php
+++ functions.php
@@ -360,7 +360,7 @@ function send_mails_on_publish( $new_status, $old_status, $post ) {
   if ( 'publish' !== $new_status or 'publish' === $old_status or 'nedm-surveys' !== get_post_type( $post ) )
 
     return;
-      $to = 'NEDM@teq.com';
+      $to = 'InfrastructureTeam@teq.com';
       $headers = 'CC: jay@teq.com.com, paulprincipato@teq.com';
       $body = sprintf( 'Hey there is a new entry!' . "\n\n");
       $body .= sprintf( 'See <%s>', get_permalink( $post ));
